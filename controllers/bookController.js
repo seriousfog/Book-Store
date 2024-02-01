8fc9a49bd6d0dc7bd7cc99251983d9bea2a98fb4
t9uo <t9uo@fresnou.org>
2024-02-01 09:29:49 -0800

    a

---
+++ controllers/bookController.js
@@ -0,0 +1,52 @@
+const {Book} = require('../models')
+const genres = ['Drama', 'Fantasy', 'Fiction', 'Nonfiction', 'Horror', 'Humor', 'Mystery', 'Bildungsroman'].sort()
+//view all
+module.exports.viewAll = async function (req, res) {
+    const books = await Book.findAll();
+    res.render('book/view_all', {books});
+};
+//profile
+module.exports.viewProfile= async function(req,res){
+    const book = await Book.findByPk(req.params.id);
+    res.render('book/profile', {book})
+}
+
+//render add form
+module.exports.renderAddForm = function(req, res){
+    const book = {
+        title: '',
+        publisher: '',
+        genre: genres[0],
+        pagenumbers: '',
+        image: '',
+        description: '',
+    }
+    res.render('book/add', {book, genres})
+}
+
+//add
+
+//render edit form
+module.exports.renderEditForm = async function(req, res){
+    const book = await Book.findByPk(req.params.id);
+    res.render('book/edit', {book, genres})
+}
+
+//update
+module.exports.updateBook = async function(req, res){
+    const book = await Book.update({
+        title: req.body.title,
+        publisher: req.body.publisher,
+        genre: req.body.genre,
+        pagenumbers: req.body.pagenumbers,
+        image: req.body.image,
+        description: req.body.description
+        }, {
+        where: {
+            id: req.params.id
+        }
+        });
+    res.redirect(`/books/profile/${req.params.id}`);
+}
+
+//delete
--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,8 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/books', bookController.viewAll)
+router.get('/books/profile/:id', bookController.viewProfile)
+router.get('/books/edit/:id', bookController.renderEditForm)
+router.post('/books/edit/:id', bookController.updateBook)
+router.get('/books/add', bookController.renderAddForm)
 module.exports = router;
